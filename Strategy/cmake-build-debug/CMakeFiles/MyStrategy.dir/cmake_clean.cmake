b82ba1a1f15d8ea7602745156f598bb5dca1ffe4
Arseny Staroverov <arsen2001god@bk.ru>
2020-04-09 20:48:33 +0300

    Another Try

---
+++ Strategy/cmake-build-debug/CMakeFiles/MyStrategy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyStrategy.dir/main.cpp.obj"
+  "MyStrategy.exe"
+  "MyStrategy.exe.manifest"
+  "MyStrategy.pdb"
+  "libMyStrategy.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyStrategy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
